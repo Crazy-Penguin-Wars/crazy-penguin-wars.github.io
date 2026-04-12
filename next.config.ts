04ad8b51803f0cd7af4f38cb4eb53122da15bcca
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2026-04-12 20:36:05 +0200

    Move up one level + github workflow

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig = {
+  output: 'export',
+  basePath: '/crazy-penguin-wars.github.io',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
