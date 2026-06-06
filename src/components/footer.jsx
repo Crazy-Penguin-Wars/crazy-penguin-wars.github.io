f95fb3bb70b43e549968bbdafb8d4eb19f572123
Cnnn666v2 <sitemakerugupl@gmail.com>
2026-06-06 17:18:43 +0200

    changes to homepage

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,40 +1,41 @@
 import {Link} from "react-router";
+import './footer.css';
 
 // TODO: MOVE CSS TO A SEPERATE FILE
 export default function Footer() {
     return (
-        <footer className={"flex flex-col items-start p-4  bg-linear-to-t from-blue-500 to-blue-700 border-t-6 border-blue-900 text-white gap-8"}>
+        <footer>
             <div className="flex flex-col gap-8">
                 <div className="flex flex-row gap-12 w-full">
-                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-300 w-1/5"}>
+                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-400 w-1/5"}>
                         <h5>Community:</h5>
                         <ul>
-                            <li><Link to={"/"}>Discord</Link></li>
+                            <li><Link to={"https://discord.gg/gnqZfsJa5j"}>Discord</Link></li>
                             <li><Link to={"/"}>YouTube</Link></li>
                             <li><Link to={"/"}>Facebook</Link></li>
                         </ul>
                     </div>
 
-                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-300 w-1/5"}>
+                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-400 w-1/5"}>
                         <h5>About:</h5>
                         <ul>
-                            <li><Link to={"/"}>About the project</Link></li>
-                            <li><Link to={"/"}>Meet the team</Link></li>
-                            <li><Link to={"/"}>F.A.Q</Link></li>
+                            <li><Link to={"/about"}>About the project</Link></li>
+                            <li><Link to={"/about/team"}>Meet the team</Link></li>
+                            <li><Link to={"/about/faq"}>F.A.Q</Link></li>
                         </ul>
                     </div>
 
-                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-300 w-1/5"}>
+                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-400 w-1/5"}>
                         <h5>External links:</h5>
                         <ul>
-                            <li><Link to={"/"}>GitHub</Link></li>
+                            <li><Link to={"https://github.com/Crazy-Penguin-Wars"}>GitHub</Link></li>
                         </ul>
                     </div>
 
-                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-300"}>
+                    <div className={"flex flex-col bg-[#02ba47] px-4 py-2 rounded-xl border-t-4 border-green-400"}>
                         <h5>Contact:</h5>
                         <ul>
-                            <li>Email - support@example.com</li>
+                            <li><Link to={"mailto:mail@crazypenguinwars.app"}>Email - mail@crazypenguinwars.app</Link></li>
                         </ul>
                     </div>
                 </div>
@@ -43,7 +44,7 @@ export default function Footer() {
                     <div className="flex flex-col justify-between">
                         <p className={"text-base!"}>© 2026 CrazyPenguinWars</p>
                         <p className={"text-base!"}>This is a fan-made revival project, we are not affiliated to Rockyou or Digital Chocolate in any way. All rights belong to their respective owners.</p>
-                        <p className={"text-base!"}>Website created and designed by: <span className={"text-yellow-400"}>Dozy Pleng</span> · <Link to={"github.com"} className={"text-green-400"}>GitHub</Link> · <Link to={"github.com"} className={"text-cyan-300"}>BlueSky</Link> · <Link to={"github.com"} className={"text-red-300"}>YouTube</Link></p>
+                        <p className={"text-base!"}>Website created and designed by: <Link to={"https://cnnn666.mandarynki.eu"} className={"text-yellow-400 hover:text-yellow-300"}>Dozy Pleng</Link> · <Link to={"https://github.com/cnnn666v2"} className={"text-green-400 hover:text-green-300"}>GitHub</Link> · <Link to={"https://bsky.app/profile/cnnn666.mandarynki.eu"} className={"text-blue-300 hover:text-cyan-300"}>BlueSky</Link> · <Link to={"https://www.youtube.com/@dozypleng"} className={"text-red-300 hover:text-red-200"}>YouTube</Link></p>
                     </div>
                 </div>
             </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,8 @@ import {Link} from "react-router";
 
 export default function Home() {
     return (
-        <div id={"container"} className="flex flex-col">
-
-            <div className={"flex flex-row pt-2 pb-4 bg-[url(/src/assets/homepage/banner.jpg)] bg-no-repeat bg-cover text-white border-b-6 border-gray-800"}>
+        <main id={"container"} className="flex flex-col">
+            <header className={"flex flex-row pt-2 pb-4 bg-[url(/homepage/banner.jpg)] bg-fixed bg-contain text-white border-b-6 border-gray-800"}>
                 <div className={"flex flex-col items-center w-full gap-4"}>
                     <h2><span className={"text-green-400"}>Crazy</span><span className={"text-white"}>Penguin</span><span className={"text-orange-400"}>Wars</span> - <span className={"text-blue-400"}>Revived</span></h2>
                     <h6>The original Facebook game brought back to life!</h6>
@@ -12,105 +11,109 @@ export default function Home() {
                         <Link to={"/"} className="btn">
                             <span>PLAY NOW</span>
                         </Link>
-                        <Link to={"/"} className="btn-action">
+                        <Link to={"https://discord.gg/gnqZfsJa5j"} className="btn-action">
                             <span>JOIN DISCORD</span>
                         </Link>
                     </div>
                 </div>
-            </div>
+            </header>
 
             <hr className={"my-4 w-1/4 self-center border-2 rounded-xl"} />
 
-            <div className={"flex flex-row w-full gap-8 mb-6 px-6"}>
+            <section className={"flex flex-row w-full gap-8 mb-6 px-6"}>
                 <div className={"flex flex-col w-fit gap-8"}>
                     <div className={"flex flex-row gap-4"}>
                         <div className={"flex flex-col"}>
                             <h2>Original Digital Chococlate game!</h2>
                             <p>CrazyPenguinWars is another game from Digital Chocolate from the "Crazy Penguin" series! The game was a browser-based, free-to-play worms-like game enjoyed by thousands online. The servers unfortunately got shut down with no option for players to play the game offline, so after almost a decade since the game's shut down we're reviving it!</p>
                         </div>
-                        <img src={"/src/assets/homepage/battle.png"} width={512} alt={"article preview image"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
+                        <img src={"/homepage/battle.png"} width={512} alt={"article preview image"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
                     </div>
-                    <hr className={"w-1/4 self-center border-2 rounded-xl\""} />
+
+                    <hr className={"w-1/4 self-center border-2 rounded-xl"} />
+
                     <div className={"flex flex-row gap-4"}>
-                        <img src={"/src/assets/homepage/battle.png"} width={512} alt={"article preview image"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
+                        <img src={"/homepage/battle.png"} width={512}  alt={"article preview image"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
                         <div className={"flex flex-col"}>
                             <h2>Revival progress</h2>
                             <p>While we are constantly working on making the game playable, we are still lacking tons of features. Right now it's possible to play versus bots, but in the future we will want to have things like proper matchmaking, customization, private lobbies, etc.<br /><br />
                                 That's the reason as to why we're looking for people like YOU to help us revive the game!</p>
                         </div>
                     </div>
-                    <hr className={"w-1/4 self-center border-2 rounded-xl\""} />
+
+                    <hr className={"w-1/4 self-center border-2 rounded-xl"} />
+
                     <div className={"flex flex-row gap-4"}>
                         <div className={"flex flex-col"}>
                             <h2>Dedicated community of fans</h2>
                             <p>We're people from all around the world wanting to experience the game one more time. We will not let this, or any other forgotten game just die! We support many other similiar projects, focused on bringing back different games from different eras and making sure they will be playable in the future as well!</p>
                         </div>
-                        <img src={"/src/assets/homepage/battle.png"} width={512} alt={"article preview image"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
+                        <img src={"/homepage/battle.png"} width={512} alt={"article preview image"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
                     </div>
                 </div>
 
-                <div className={"w-[40%] flex flex-col self-center rounded-xl items-end gap-2 sticky! top-1 mb-auto"}>
+                <aside className={"w-[40%] flex flex-col self-center rounded-xl items-end gap-2 sticky! top-1 mb-auto"}>
                     <h2>Our discord server:</h2>
-                    <iframe src="https://discord.com/widget?id=795676694161784922&theme=dark" width="350" height="500"
+                    <iframe src="https://discord.com/widget?id=837664482670411816&theme=dark" width="350" height="500"
                             allowTransparency="true" frameBorder="0"
                             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                </div>
-            </div>
+                </aside>
+            </section>
 
             <hr className={"my-4 w-1/4 self-center border-2 rounded-xl"}/>
 
-            <div className={"flex flex-row w-full justify-center gap-8 text-white mb-6 px-6"}>
+            <section className={"flex flex-row w-full justify-center gap-8 text-white mb-6 px-6"}>
                 <div className={"flex flex-col items-center text-center bg-red-600 border-red-400 border-t-6 border-r-6 w-1/3 rounded-xl p-4 gap-4"}>
                     <h4 className={"uppercase"}>Fight your friends!</h4>
                     <p className={"-mt-6"}>(matchmaking included)</p>
-                    <img src={"/src/assets/homepage/battle.png"} alt="Battle" className={"rounded-lg border-2 mt-auto"} />
+                    <img src={"/homepage/battle.png"} alt="Battle" className={"rounded-lg border-2 mt-auto"} />
                     <hr className={"w-3/4 self-center border-4 rounded-xl border-t-red-400 border-b-red-700 border-l-red-700 border-r-red-400"} />
                     <p>Got a beef that needs to be settled? Take out your friend for a 1v1 and see who's right!</p>
                 </div>
 
                 <div className={"flex flex-col items-center text-center bg-green-800 border-green-600 border-t-6 border-r-6 w-1/3 rounded-xl p-4 gap-4"}>
                     <h4 className={"uppercase"}>Crazy weapons!</h4>
-                    <img src={"/src/assets/homepage/weapon.png"} alt="Battle" className={"rounded-lg border-2 mt-auto"} />
+                    <img src={"/homepage/weapon.png"} alt="Battle" className={"rounded-lg border-2 mt-auto"} />
                     <hr className={"w-3/4 self-center border-4 rounded-xl border-t-green-600 border-b-green-900 border-l-green-900 border-r-green-600"} />
                     <p>Tired of running with just an AK? Choose a bazooka, frag grenade, SMG or something else!</p>
                 </div>
 
                 <div className={"flex flex-col items-center text-center bg-purple-600 border-purple-400 border-t-6 border-r-6 w-1/3 rounded-xl p-4 gap-4"}>
                     <h4 className={"uppercase"}>Customize your looks!</h4>
-                    <img src={"/src/assets/homepage/customize.png"} alt="Battle" className={"rounded-lg border-2 mt-auto"} />
+                    <img src={"/homepage/customize.png"} alt="Battle" className={"rounded-lg border-2 mt-auto"} />
                     <hr className={"w-3/4 self-center border-4 rounded-xl border-t-purple-400 border-b-purple-700 border-l-purple-700 border-r-purple-400"} />
                     <p>Feeling like a soldier? Or maybe a wizard? Choose from tons of clothing options to match your vibe!</p>
                 </div>
-            </div>
+            </section>
 
             <hr className={"mt-4 mb-8 w-1/4 self-center border-2 rounded-xl"}/>
 
             {/*TODO: TURN THIS INTO A COMPONENT */}
-            <div className={"flex flex-col bg-cyan-600 border-t-6 border-cyan-400 p-4 justify-center items-center w-full gap-4 text-white"}>
+            <section className={"flex flex-col bg-cyan-600 border-t-6 border-cyan-400 p-4 justify-center items-center w-full gap-4 text-white"}>
                 <h4>Latest news:</h4>
                 <div className={"flex flex-row gap-4 w-full justify-center items-center"}>
-                    <div className={"flex flex-col w-full gap-4 bg-blue-600 border-6 border-blue-300 p-4 rounded-xl basis-1/3"}>
+                    <article className={"flex flex-col w-full gap-4 bg-blue-600 border-6 border-blue-300 p-4 rounded-xl basis-1/3"}>
                         <h5>Headline 1</h5>
-                        <img src={"/src/assets/homepage/battle.png"} alt={"article preview image"} className={"w-full border-2 rounded-xl"} />
+                        <img src={"/homepage/battle.png"} alt={"article preview image"} className={"w-full border-2 rounded-xl"} />
                         <p>Beginning of the news papaer lmfao among us sus article long text just testingBeginning of the news papaer lmfao among us sus article long text just testingBeginning...</p>
                         <Link to={"/blog/1"} className={"btn w-fit!"}><span>Read more</span></Link>
-                    </div>
+                    </article>
 
-                    <div className={"flex flex-col w-full gap-4 bg-blue-600 border-6 border-blue-300 p-4 rounded-xl basis-1/3"}>
+                    <article className={"flex flex-col w-full gap-4 bg-blue-600 border-6 border-blue-300 p-4 rounded-xl basis-1/3"}>
                         <h5>Headline 1</h5>
-                        <img src={"/src/assets/homepage/battle.png"} alt={"article preview image"} className={"w-full border-2 rounded-xl"} />
+                        <img src={"/homepage/battle.png"} alt={"article preview image"} className={"w-full border-2 rounded-xl"} />
                         <p>Beginning of the news papaer lmfao among us sus article long text just testingBeginning of the news papaer lmfao among us sus article long text just testingBeginning...</p>
                         <Link to={"/blog/1"} className={"btn w-fit!"}><span>Read more</span></Link>
-                    </div>
+                    </article>
 
-                    <div className={"flex flex-col w-full gap-4 bg-blue-600 border-6 border-blue-300 p-4 rounded-xl basis-1/3"}>
+                    <article className={"flex flex-col w-full gap-4 bg-blue-600 border-6 border-blue-300 p-4 rounded-xl basis-1/3"}>
                         <h5>Headline 1</h5>
-                        <img src={"/src/assets/homepage/battle.png"} alt={"article preview image"} className={"w-full border-2 rounded-xl"} />
+                        <img src={"/homepage/battle.png"} alt={"article preview image"} className={"w-full border-2 rounded-xl"} />
                         <p>Beginning of the news papaer lmfao among us sus article long text just testingBeginning of the news papaer lmfao among us sus article long text just testingBeginning...</p>
                         <Link to={"/blog/1"} className={"btn w-fit!"}><span>Read more</span></Link>
-                    </div>
+                    </article>
                 </div>
-            </div>
-        </div>
+            </section>
+        </main>
     );
 }
