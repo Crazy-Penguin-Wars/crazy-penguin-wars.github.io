d969a99d193e81f3a5550e0c12e182224982b0f2
Cnnn666v2 <sitemakerugupl@gmail.com>
2026-06-18 16:39:58 +0200

    responsive "play" page

--- src/pages/play.jsx
+++ src/pages/play.jsx
@@ -14,7 +14,7 @@ export default function Play() {
                     <img src={"/play/menu.png"} width={512} alt={"game screenshot"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
                     <img src={"/play/gameplay.png"} width={512} alt={"game screenshot"} className={"border-2 border-blue-600 my-2 rounded-xl"} />
                 </div>
-                <Link to="https://github.com/Crazy-Penguin-Wars/cpw-launcher/releases/download/1.0.0/Crazy.Penguin.Wars.1_0_0.zip" className={"btn-action text-xl mt-auto"}><span>DOWNLOAD</span></Link>
+                <Link to="https://github.com/Crazy-Penguin-Wars/cpw-launcher/releases/download/1.0.0/Crazy.Penguin.Wars.1_0_0.zip" className={"btn-action text-xl mt-auto"}><span>DOWNLOAD <span className={"mt-auto ml-1 text-sm"}>(windows only)</span></span></Link>
                 <p>Latest release: 1.0.0 · <Link to={"https://github.com/Crazy-Penguin-Wars/cpw-launcher/releases/"} className={"text-blue-400! hover:text-blue-600! transition-colors ease-in-out"}>version history</Link></p>
             </div>
 
